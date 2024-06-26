c62331f0c738a833ce0602a23af78c1122138956
Khoi Do <khoizpro31@gmail.com>
2024-06-26 08:21:58 +0700

    Change deprecation domain configuration to remotePattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "uploadthing.com",
-      "lh3.googleusercontent.com",
-      "utfs.io",
-      "avatars.githubusercontent.com",
+    // domains: [
+    //   "uploadthing.com",
+    //   "lh3.googleusercontent.com",
+    //   "utfs.io",
+    //   "avatars.githubusercontent.com",
+    // ],
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
     ],
   },
 };
