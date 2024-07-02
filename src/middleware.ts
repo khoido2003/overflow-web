affc201e01e3b7001cee8cb9372fd29aad1d8463
Khoi Do <khoizpro31@gmail.com>
2024-07-03 00:31:56 +0700

    Fixing middleware to protect route

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,13 +14,19 @@ export default auth((req) => {
   // Current url
   const { nextUrl } = req;
 
+  if (nextUrl.pathname === "/") return null;
+
   // Check if there is a session token
   const isLoggedIn = !!req.auth;
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isPublicRoute = publicRoutes.some((route) =>
-    nextUrl.pathname.startsWith(nextUrl.pathname),
-  );
+  const isPublicRoute = publicRoutes.some((route) => {
+    return nextUrl.pathname.startsWith(route);
+  });
+
+  // console.log(isPublicRoute);
+  // console.log(nextUrl.pathname);
+
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
   // If in login or register, then no redirect
@@ -38,7 +44,7 @@ export default auth((req) => {
 
   // User access protected route but did not login then redirect them to login page
   if (!isLoggedIn && !isPublicRoute) {
-    // return Response.redirect(new URL("/auth/login", nextUrl));
+    return Response.redirect(new URL("/auth/login", nextUrl));
   }
 
   return null;
